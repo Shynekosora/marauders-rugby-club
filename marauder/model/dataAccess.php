47bf342f0cfd28d203c9f8f3b5bff82ff7ac9eb5
ngoulongkam <25244131+ngoulongkam@users.noreply.github.com>
2020-03-14 16:18:01 +0000

    extract database host into varible for easy reading

--- marauder/model/dataAccess.php
+++ marauder/model/dataAccess.php
@@ -3,11 +3,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$db_host = "kunet";
 $db_user = "k1811423";
 $db_name = "db_k1811423";
 $db_password = "12345678";
 
-$pdo = new PDO("mysql:host=kunet;dbname=$db_name", $db_user, $db_password);
+$pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
